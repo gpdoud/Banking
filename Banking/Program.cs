d4a31a7b259e14c7dcaa23b99ce7baef798bcecf
Greg doud <gpdoud@gmail.com>
2017-09-07 16:45:29 -0400

    Added account class

--- Banking/Program.cs
+++ Banking/Program.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Banking {
 	class Program {
+
+		void Run() {
+			Account checking = new Account();
+			checking.SetName("Checking 1");
+			checking.Withdraw(100.00);
+			checking.Deposit(50.00);
+			checking.Deposit(-100.00);
+			Console.WriteLine($"Account {checking.GetName()}, balance is {checking.CheckBalance()}");
+		}
+
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
