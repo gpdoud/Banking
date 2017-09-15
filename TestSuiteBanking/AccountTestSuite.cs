6227520c12ddb2cd3876627b03fb0da4c10e7866
Greg doud <gpdoud@gmail.com>
2017-09-15 13:27:09 -0400

    Added some test cases on Account

---
+++ TestSuiteBanking/AccountTestSuite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaxTechnicalTraining.BankingLibrary;
+
+namespace TestSuiteBanking {
+
+	[TestClass]
+	public class AccountTestSuite {
+
+		[TestMethod]
+		public void TestInstantiation() {
+			Account a = new Account();
+			Assert.IsInstanceOfType(a, typeof(Account), "Default constructor");
+
+			Assert.AreEqual(96434, a.getNumber(), "Account number is 96434");
+
+			a = new Account("Test Account");
+			Assert.IsInstanceOfType(a, typeof(Account), "Single string constructor");
+
+			Assert.AreEqual(0, a.CheckBalance(), "Balance is zero");
+
+			Assert.AreEqual(96434 + 74747, a.getNumber(), "Account number is 96434+74747");
+		}
+	}
+}
---
+++ TestSuiteBanking/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestSuiteBanking")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSuiteBanking")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0631600f-3779-4f52-b0b2-bfc9545398cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
